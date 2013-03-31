1b112aacd0722e10353a8f3f931fb0c4f571d54a
Robert Wertman <robert.wertman@gmail.com>
2013-03-30 22:37:45 -0400

    Removed database credentials.

--- db.go
+++ db.go
@@ -4,8 +4,12 @@ import "github.com/gosexy/db"
 
 // Database connection settings.
 var dbSettings = db.DataSource{
-	Host:     "localhost",
-	Database: "cs495_db1",
-	User:     "cs495st1",
-	Password: "cal_b6Y%dV",
+	Host:     "#####",
+	Database: "#####",
+	User:     "#####",
+	Password: "#####",
+}
+
+func init() {
+	panic("Update dbSettings in db.go")
 }
