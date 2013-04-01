c8faa1d5cc7a4967ca10a6a6717b7910b8cc5324
Robert Wertman <robert.wertman@gmail.com>
2013-04-01 13:38:59 -0400

    Ensuring proper database connection at startup.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"github.com/gosexy/db"
 	_ "github.com/gosexy/db/mysql"
 	"net/http"
@@ -13,10 +14,18 @@ func main() {
 	if err != nil {
 		// If it didn't work, quit and tell us about it.
 		panic(err)
-	} else {
-		// Otherwise, close the connection when we exit.
-		defer sess.Close()
 	}
+
+	// Make sure we actually connected my executing a command.
+	drv := sess.Driver().(*sql.DB)
+	_, err = drv.Exec("SHOW TABLES;")
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Otherwise, close the connection when we exit.
+	defer sess.Close()
 
 	// Register the api server hooks on our HTTP server
 	runApiServer(sess)
