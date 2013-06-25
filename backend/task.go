c3e1275798290f3e1a879a43aa679c05dfb52eb1
Robert Wertman <robert.wertman@gmail.com>
2013-06-25 18:17:04 -0400

    We can now detect free ranges of time.

--- backend/task.go
+++ backend/task.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+"fmt"
 	"github.com/gosexy/db"
 	"net/http"
 	"strconv"
@@ -48,13 +49,13 @@ func createTask(res http.ResponseWriter, req *http.Request, sess db.Database) ap
 			errFields = append(errFields, "duration")
 			errMsgs = append(errMsgs, "Please pick a proper duration.")
 		} else {
-			task["timeRequired"] = hours * 60 + mins
+			task["timeRequired"] = hours*60 + mins
 		}
 	}
 
 	dueStr := req.FormValue("dueDate_submit") + " " + req.FormValue("dueTime")
 	dueDate, err := time.Parse(formFormat, dueStr)
-	task["dueDate"] = dueDate
+	task["dueDate"] = dueDate.Format(timeFormat)
 
 	if err != nil {
 		errFields = append(errFields, "dueDate")
@@ -66,10 +67,105 @@ func createTask(res http.ResponseWriter, req *http.Request, sess db.Database) ap
 	}
 
 	taskTable := sess.ExistentCollection("Tasks")
-	_, err = taskTable.Append(task)
+	id, err := taskTable.Append(task)
 	if err != nil {
 		println(err.Error())
 		return &apiFormResponse{false, nil, nil}
 	}
+	createTaskInstances(id, sess)
 	return &apiFormResponse{true, nil, nil}
+}
+
+func createTaskInstances(ids []db.Id, sess db.Database) {
+	tasks := sess.ExistentCollection("Tasks")
+
+	for _, id := range ids {
+		task, err := tasks.Find(db.Cond{"tid": id})
+		if err != nil {
+			return
+		}
+
+		startTime := time.Now()
+		fmt.Println(startTime)
+		endTime, err := time.Parse(timeFormat, task["duedate"].(string))
+
+		events, _ := getEventsInRange(int(task["creator"].(int64)), startTime, endTime, sess)
+
+		availability := [][2]time.Time{{startTime, endTime}}
+
+		fmt.Println(availability)
+
+		taskEvents:
+		for _, event := range events {
+			fmt.Println(event.Name, event.StartTime, event.StartTime.Add(event.Duration))
+			eventSections:
+			for i, section := range availability {
+				if section[0].Before(event.StartTime) {
+					// We need to make sure the end of the event doesn't
+					// intersect with the start of the section.
+					// |---- Section ---?--
+					//    ?--- Event ---?--
+					if section[1].Before(event.StartTime) {
+						// If we're just after the section entirely, skip this
+						// iteration.
+						// |---- Section ----|
+						//                     |--- Event ---?--
+						continue eventSections
+					} else {
+						// |---- Section ----|
+						//     |--- Event ---?--
+						if section[1].After(event.StartTime.Add(event.Duration)) {
+							// Here, the event is contained entirely within the
+							// section.
+							// |---- Section ----|
+							//   |--- Event ---|
+							// Turns into
+							// |-|             |-|
+							availability = append(availability[:i], append([][2]time.Time{{section[0], event.StartTime}, {event.StartTime.Add(event.Duration), section[1]}}, availability[i+1:]...)...)
+							continue taskEvents
+						} else {
+							// Otherwise, the event cuts off the remaining
+							// time, and may cut into the next sections' times.
+							// |---- Section ----|
+							//     |--- Event ---?--
+							// turns into
+							// |---|
+							availability[i][1] = event.StartTime
+							continue eventSections
+						}
+					}
+				} else {
+					// This means the event starts before the section starts.
+					// We need to see if it ends before it begins as well.
+					//         ?---- Section ----|
+					// |--- Event ---?--
+					if section[0].Before(event.StartTime.Add(event.Duration)) {
+						// If it does, none of the remaining sections will
+						// overlap, and we can continue on to the next event.
+						//                 |---- Section ----|
+						// |--- Event ---|
+						continue taskEvents
+					} else if section[1].After(event.StartTime.Add(event.Duration)) {
+						// This means the event intersects the section in the
+						// following manner:
+						//     |---- Section ----|
+						// |--- Event ---|
+						// turns into
+						//               |-------|
+						availability[i][0] = event.StartTime.Add(event.Duration)
+						continue taskEvents
+					} else {
+						// This means the event takes up the entire section,
+						// and we should remove it.
+						//   |---- Section ----|
+						// |------- Event -------|
+						availability = append(availability[:i], availability[i+1:]...)
+						continue eventSections
+					}
+				}
+			}
+		}
+
+		fmt.Println(availability)
+	}
 }
