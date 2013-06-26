edc2a9981b83ec76b2bcff3c4bdce6cc739bb468
Robert Wertman <robert.wertman@gmail.com>
2013-06-26 05:37:26 -0400

    Now we can mask out times that we sleep.

--- backend/task.go
+++ backend/task.go
@@ -86,86 +86,21 @@ func createTaskInstances(ids []db.Id, sess db.Database) {
 		}
 
 		startTime := time.Now()
-		fmt.Println(startTime)
 		endTime, err := time.Parse(timeFormat, task["duedate"].(string))
 
 		events, _ := getEventsInRange(int(task["creator"].(int64)), startTime, endTime, sess)
 
-		availability := [][2]time.Time{{startTime, endTime}}
+		availability := NewSchedule(startTime, endTime)
 
-		fmt.Println(availability)
+		fmt.Println(availability.String())
 
-		taskEvents:
 		for _, event := range events {
 			fmt.Println(event.Name, event.StartTime, event.StartTime.Add(event.Duration))
-			eventSections:
-			for i, section := range availability {
-				if section[0].Before(event.StartTime) {
-					// We need to make sure the end of the event doesn't
-					// intersect with the start of the section.
-					// |---- Section ---?--
-					//    ?--- Event ---?--
-					if section[1].Before(event.StartTime) {
-						// If we're just after the section entirely, skip this
-						// iteration.
-						// |---- Section ----|
-						//                     |--- Event ---?--
-						continue eventSections
-					} else {
-						// |---- Section ----|
-						//     |--- Event ---?--
-						if section[1].After(event.StartTime.Add(event.Duration)) {
-							// Here, the event is contained entirely within the
-							// section.
-							// |---- Section ----|
-							//   |--- Event ---|
-							// Turns into
-							// |-|             |-|
-							availability = append(availability[:i], append([][2]time.Time{{section[0], event.StartTime}, {event.StartTime.Add(event.Duration), section[1]}}, availability[i+1:]...)...)
-							continue taskEvents
-						} else {
-							// Otherwise, the event cuts off the remaining
-							// time, and may cut into the next sections' times.
-							// |---- Section ----|
-							//     |--- Event ---?--
-							// turns into
-							// |---|
-							availability[i][1] = event.StartTime
-							continue eventSections
-						}
-					}
-				} else {
-					// This means the event starts before the section starts.
-					// We need to see if it ends before it begins as well.
-					//         ?---- Section ----|
-					// |--- Event ---?--
-					if section[0].Before(event.StartTime.Add(event.Duration)) {
-						// If it does, none of the remaining sections will
-						// overlap, and we can continue on to the next event.
-						//                 |---- Section ----|
-						// |--- Event ---|
-						continue taskEvents
-					} else if section[1].After(event.StartTime.Add(event.Duration)) {
-						// This means the event intersects the section in the
-						// following manner:
-						//     |---- Section ----|
-						// |--- Event ---|
-						// turns into
-						//               |-------|
-						availability[i][0] = event.StartTime.Add(event.Duration)
-						continue taskEvents
-					} else {
-						// This means the event takes up the entire section,
-						// and we should remove it.
-						//   |---- Section ----|
-						// |------- Event -------|
-						availability = append(availability[:i], availability[i+1:]...)
-						continue eventSections
-					}
-				}
-			}
+			availability.Subtract(event)
 		}
 
-		fmt.Println(availability)
+		availability.FilterTimes(9, 17)
+
+		fmt.Println(availability.String())
 	}
 }
